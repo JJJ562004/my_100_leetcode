971505e5d8dd0e8f812b156add4a5663bb8820ec
JJJ4065 <darknessrisesgamer999@gmail.com>
2025-04-01 20:35:27 +0700

    update

--- github_leetcode.py
+++ github_leetcode.py
@@ -423,3 +423,6 @@ def reverse(self, x: int) -> int:
     sign = -1 if x < 0 else 1
     rev = int(str(abs(x))[::-1]) * sign
     return rev if -(2**31) <= rev <= (2**31 - 1) else 0
+#709. To lower case
+def toLowerCase(self, s: str) -> str:
+        return s.lower()
